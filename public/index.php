bf9bdd631812774114085200c162599e31bdfc4c
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-21 13:27:40 +0100

    pdo connect + test

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php 
 
 require_once '../config.php';
-require_once '../model/contactmod.php';
-
 
 try {
-     $pdo = new PDO("mysql:host=DB_HOST;dbname=DB_NAME;port=DB_PORT", DB_LOGIN, DB_PWD);
-} catch (Exception $e) {
-     die($e->getMessage());
-}
-
+     $db = new PDO ('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME,DB_USER, DB_PWD);
+     $db->exec("SET CHARACTER SET utf8");
+     require_once '../model/contactmod.php';
+ }catch(PDOException $e){
+         die($e->getMessage());
+ }
 
 include '../view/homepageview.php';
